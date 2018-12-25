1d21617d28db9b1640731f6923028e6ae5591aed
FilippKramer <krame490@gmail.com>
2018-12-25 17:40:48 -0600

    Retrieve cross-domain html with request-promise and add error handling

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const app = require("express")();
 const http = require("http").Server(app);
 const rp = require('request-promise');
 const url = "https://en.wikipedia.org/wiki/President_of_the_United_States";
+const url2 = "asdfadsfadsf";
 
-app.get("/", (req, res) => res.send("<h1>Hello world!</h1>"));
+app.get("/", (req, res) => {
+  rp(url)
+    .then(html => res.send(html))
+    .catch(err => {
+      console.log(err)
+      res.send("Invalid URL")
+    })
+})
 
 http.listen(3000, () => console.log("listening on port 3000"));
